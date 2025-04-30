dccef640f0edd79c03873df5806c0b26bc78fb9e
Guilherme Santos <guilhermealbertin20@gmail.com>
2025-04-30 16:55:27 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
     const elements = document.querySelectorAll(".fade-in, .slide-up");
+const hamburger = document.getElementById('hamburger');
+    const navLinks = document.getElementById('nav-links');
 
+    hamburger.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
     const observer = new IntersectionObserver(
         (entries) => {
             entries.forEach((entry) => {
